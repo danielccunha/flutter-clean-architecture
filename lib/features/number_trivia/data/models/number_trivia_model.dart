e62142e28b0bd4388a6ed42bfcdfe23141e42f27
Daniel Cunha <danielcunha54@gmail.com>
2020-11-05 14:47:28 -0300

    feat: create NumberTriviaModel

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:meta/meta.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  const NumberTriviaModel({
+    @required String text,
+    @required int number,
+  }) : super(text: text, number: number);
+
+  factory NumberTriviaModel.fromJSON(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      text: json['text'] as String,
+      number: (json['number'] as num).toInt(),
+    );
+  }
+
+  Map<String, dynamic> toJSON() {
+    return {
+      'text': text,
+      'number': number,
+    };
+  }
+}
