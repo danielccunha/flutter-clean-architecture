3a9b0a63f9ba7e71c74c79559b55debb5f81b3e0
Daniel Cunha <danielcunha54@gmail.com>
2020-11-05 21:33:35 -0300

    refactor: add const constructors for failures

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,13 +1,19 @@
 import 'package:equatable/equatable.dart';
 
-abstract class Failure extends Equatable {}
+abstract class Failure extends Equatable {
+  const Failure();
+}
 
 class ServerFailure extends Failure {
   @override
   List<Object> get props => [];
+
+  const ServerFailure();
 }
 
 class CacheFailure extends Failure {
   @override
   List<Object> get props => [];
+
+  const CacheFailure();
 }
